d3dc28bf99224415ba95b63ec444c9109121ac10
Amy Tam <atam@hmc.edu>
2021-03-06 15:55:59 -0800

    update pagila-hw2 sql

--- sql/03.sql
+++ sql/03.sql
@@ -6,7 +6,22 @@
  * In problem 16 of pagila-hw1, you ordered the films by most profitable.
  * Modify this query so that it returns only the film_id of the top 5 most profitable films.
  * This will be your subquery.
- * 
+ *
  * Next, join the film, inventory, rental, and customer tables.
  * Use a where clause to restrict results to the subquery.
  */
+
+SELECT DISTINCT customer_id FROM customer
+JOIN rental USING(customer_id)
+JOIN inventory USING(inventory_id)
+JOIN film USING(film_id)
+WHERE film_id IN (
+  SELECT film_id FROM film
+  JOIN inventory USING(film_id)
+  JOIN rental USING(inventory_id)
+  JOIN payment USING(rental_id)
+  GROUP BY film_id
+  ORDER BY SUM(amount) DESC
+  LIMIT 5
+)
+ORDER BY customer_id;
