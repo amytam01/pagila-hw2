b8ebbfaf6560dc1ad6762f98b3c693f7b1ffabec
Amy <38165346+amytam01@users.noreply.github.com>
2021-03-06 16:00:04 -0800

    fixed workflow

--- sql/01.sql
+++ sql/01.sql
@@ -7,6 +7,6 @@
  * The postgresqltutorial.com website has a solution for this problem.
  */
 
-SELECT film_id, title FROM film
+SELECT film_id, title FROM film 
 WHERE rental_rate > (SELECT AVG(rental_rate) FROM film)
 ORDER BY title;
